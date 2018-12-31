97867ad0d8f8aec44d49028eebf3e187373fe6ce
Austin Gil <austin@stegosource.com>
2018-12-31 15:28:15 -0800

    Removing nuxt favicon

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -14,7 +14,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, // TODO
       { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600|Roboto Mono' }
     ]
   },
