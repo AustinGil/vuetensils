493dfc719054e25b235ae9338956e808903822d3
Austin Gil <austin@stegosource.com>
2019-01-01 21:07:20 -0800

    Getting Jest hooked up

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,62 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-	verbose: true,
-	testPathIgnorePatterns: [
-		'<rootDir>/node_modules/',
-		'<rootDir>/dist/',
-		'<rootDir>/demo/',
-		'<rootDir>/docs/'
-	],
-	moduleFileExtensions: ["js", "jsx", "json", "vue"],
-	transform: {
-		// "^.+\\.vue$": "vue-jest",
-		".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
-			"jest-transform-stub",
-		"^.+\\.jsx?$": "babel-jest",
-		"^.+\\.js?$": "babel-jest"
-	},
-	moduleNameMapper: {
-		"^@/(.*)$": "<rootDir>/src/$1"
-	},
-	// snapshotSerializers: ["jest-serializer-vue"],
-	testMatch: [
-		"**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
-	]
+  // clearMocks: true,
+  // resetMocks: false,
+  // restoreMocks: false,
+
+  // collectCoverageFrom: null,
+
+  coverageDirectory: "tests/unit/coverage",
+
+  // coveragePathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
+
+  // coverageReporters: [
+  //   "json",
+  //   "text",
+  //   "lcov",
+  //   "clover"
+  // ],
+
+  errorOnDeprecated: false,
+
+  // forceCoverageMatch: [],
+
+  // globals: {},
+
+  // moduleDirectories: [
+  //   "node_modules"
+  // ],
+
+  moduleFileExtensions: ["js", "jsx", "json", "vue"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+
+  // moduleNameMapper: {},
+
+  // snapshotSerializers: ["jest-serializer-vue"],
+
+  testMatch: [
+    "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/demo/',
+    '<rootDir>/docs/'
+  ],
+  transform: {
+    // "^.+\\.vue$": "vue-jest",
+    // ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+    // 	"jest-transform-stub",
+    "^.+\\.jsx?$": "babel-jest"
+  },
+
+  // timers: "fake",
+
+  verbose: true,
 };
--- tests/unit/specs/Fetch.spec.js
+++ tests/unit/specs/Fetch.spec.js
@@ -2,12 +2,12 @@ import { shallowMount } from "@vue/test-utils";
 import Fetch from "@/components/Fetch";
 
 describe("Fetch", () => {
-	it("renders props.msg when passed", () => {
-		// const msg = "new message";
-		// const wrapper = shallowMount(Fetch, {
-		// 	propsData: { msg }
-		// });
-		// expect(wrapper.text()).toMatch(msg);
-		expect(1 + 1).toBe(2)
-	});
+  it("renders props.msg when passed", () => {
+    // const msg = "new message";
+    // const wrapper = shallowMount(Fetch, {
+    // 	propsData: { msg }
+    // });
+    // expect(wrapper.text()).toMatch(msg);
+    expect(1 + 1).toBe(2)
+  });
 });
