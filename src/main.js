989dee91825d459f37bd3a88816d6aa3c9f935f2
Austin Gil <austin@stegosource.com>
2019-01-05 19:51:56 -0800

    Smaller bundle size

--- src/main.js
+++ src/main.js
@@ -1,71 +1,23 @@
-import Alert from "./components/Alert";
-import Drawer from "./components/Drawer";
-import Dropdown from "./components/Dropdown";
-import Fetch from "./components/Fetch";
+// import Alert from "./components/Alert"
+import Drawer from "./components/Drawer/index"
+import Dropdown from "./components/Dropdown/index"
+import Fetch from "./components/Fetch/index"
 // import Hidden from "./components/Hidden";
-import Intersection from "./components/Intersection";
-import Loading from "./components/Loading";
-import Modal from "./components/Modal";
+import Intersection from "./components/Intersection/index"
+import Loading from "./components/Loading/index"
+import Modal from "./components/Modal/index"
 
 // The whole shebang
 export default {
-  install (Vue, options) {
+  install(Vue, options) {
     // Components
-    Vue.component(Alert.name, Alert);
-    Vue.component(Drawer.name, Drawer);
-    Vue.component(Dropdown.name, Dropdown);
-    Vue.component(Fetch.name, Fetch);
+    // Vue.component(Alert.name, Alert)
+    Vue.component(Drawer.name, Drawer)
+    Vue.component(Dropdown.name, Dropdown)
+    Vue.component(Fetch.name, Fetch)
     // Vue.component(Hidden.name, Hidden);
-    Vue.component(Intersection.name, Intersection);
-    Vue.component(Loading.name, Loading);
-    Vue.component(Modal.name, Modal);
+    Vue.component(Intersection.name, Intersection)
+    Vue.component(Loading.name, Loading)
+    Vue.component(Modal.name, Modal)
   }
-};
-
-export const VtsAlert = {
-  install (Vue, options) {
-    Vue.component(Alert.name, Alert);
-  }
-};
-
-export const VtsDrawer = {
-  install (Vue, options) {
-    Vue.component(Drawer.name, Drawer);
-  }
-};
-
-export const VtsDropdown = {
-  install (Vue, options) {
-    Vue.component(Dropdown.name, Dropdown);
-  }
-};
-
-export const VtsFetch = {
-  install (Vue, options) {
-    Vue.component(Fetch.name, Fetch);
-  }
-};
-
-// export const VtsHidden = {
-//   install (Vue, options) {
-//     Vue.component(Hidden.name, Hidden);
-//   }
-// };
-
-export const VtsIntersection = {
-  install (Vue, options) {
-    Vue.component(Intersection.name, Intersection);
-  }
-};
-
-export const VtsLoading = {
-  install (Vue, options) {
-    Vue.component(Loading.name, Loading);
-  }
-};
-
-export const VtsModal = {
-  install (Vue, options) {
-    Vue.component(Modal.name, Modal);
-  }
-};
+}
