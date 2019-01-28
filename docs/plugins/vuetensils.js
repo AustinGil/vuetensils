902aac8176a83e1507ca4819ce64f17171094be8
Austin Gil <austin@stegosource.com>
2019-01-27 18:30:33 -0800

    Removing storybook and old docs

--- docs/plugins/vuetensils.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue"
-// import Vuetensils from "vuetensils"
-// import 'vuetensils/dist/vuetensils.min.css'
-// import Vuetensils from "../../dist/vuetensils.esm"
-// import '../../dist/vuetensils.min.css'
-import Vuetensils from "../../src/entry"
-
-Vue.use(Vuetensils)
