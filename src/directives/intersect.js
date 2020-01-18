4dd7b4eb120f2f04788099756d0c4771dc90b1e7
Austin Gil <austin@stegosource.com>
2020-01-17 22:21:42 -0800

    Formatting

--- src/directives/intersect.js
+++ src/directives/intersect.js
@@ -1,36 +1,36 @@
 export default {
-    inserted: (el, binding) => {
-        const options = {
-            ...binding.value
-        };
-	options.root = options.root && document.querySelector(options.root);
-	var onLoadCall = true;
-        const listeners = {
-            onEnter: binding.value.onEnter,
-            onLeave: binding.value.onLeave,
-            onChange: binding.value.onChange
-        }
-        
-        if(binding.value instanceof Function){
-            const { enter, leave, change } = binding.modifiers
-            listeners.onEnter = enter && binding.value;
-            listeners.onChange = change && binding.value;
-            listeners.onLeave = leave && binding.value;
-        }
-
-        const observer = new IntersectionObserver((entries) => {
-	    if(!onLoadCall){
-                const { isIntersecting } = entries[0];
-                if(isIntersecting){
-                    listeners.onEnter && listeners.onEnter(el);
-                }else{
-                    listeners.onLeave && listeners.onLeave(el);
-                }
-                listeners.onChange && listeners.onChange(el, isIntersecting);
-	    }else{
-	        onLoadCall = false;
-	    }
-        }, options);
-        observer.observe(el);
+  inserted: (el, binding) => {
+    const options = {
+      ...binding.value,
     }
+    options.root = options.root && document.querySelector(options.root)
+    var onLoadCall = true
+    const listeners = {
+      onEnter: binding.value.onEnter,
+      onLeave: binding.value.onLeave,
+      onChange: binding.value.onChange,
+    }
+
+    if (binding.value instanceof Function) {
+      const { enter, leave, change } = binding.modifiers
+      listeners.onEnter = enter && binding.value
+      listeners.onChange = change && binding.value
+      listeners.onLeave = leave && binding.value
+    }
+
+    const observer = new IntersectionObserver(entries => {
+      if (!onLoadCall) {
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          listeners.onEnter && listeners.onEnter(el)
+        } else {
+          listeners.onLeave && listeners.onLeave(el)
+        }
+        listeners.onChange && listeners.onChange(el, isIntersecting)
+      } else {
+        onLoadCall = false
+      }
+    }, options)
+    observer.observe(el)
+  },
 }
