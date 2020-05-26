2dc3462751b696e4bb006fd98ebf996de0b7de75
Austin Gil <austin@stegosource.com>
2020-05-25 21:40:50 -0700

    Getting dates sort of in order

--- src/data/keycodes.js
+++ src/data/keycodes.js
@@ -1,10 +1,14 @@
-export default {
-  ENTER: 13,
-  SPACE: 32,
+export default Object.freeze({
   TAB: 9,
+  ENTER: 13,
   ESC: 27,
+  SPACE: 32,
+  PAGEUP: 33,
+  PAGEDOWN: 34,
+  END: 35,
+  HOME: 36,
   LEFT: 37,
   UP: 38,
   RIGHT: 39,
-  DOWN: 40
-};
+  DOWN: 40,
+})
