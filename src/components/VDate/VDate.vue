2e758cbf1163b689b86f50a895086a6ae64cb221
Austin Gil <austin@stegosource.com>
2020-06-10 21:38:19 -0700

    Finishing up Date v1

--- src/components/VDate/VDate.vue
+++ src/components/VDate/VDate.vue
@@ -1,9 +1,15 @@
 <template>
-  <div :id="id" class="datepicker">
-    <!-- TODO: slot -->
-    <button aria-label="Choose Date" @click="show = !show">
-      calendar btn {{ show }}
-    </button>
+  <div :id="id" v-clickout="onClickout" :class="['vtd-date', classes.root]">
+    <slot v-bind="toggle">
+      <button
+        v-bind="toggle.bind"
+        type="button"
+        :class="['vtd-date__toggle', classes.toggle]"
+        v-on="toggle.on"
+      >
+        <span role="img" aria-label="show calendar">&#x1F4C5;</span>
+      </button>
+    </slot>
 
     <div
       v-show="show"
@@ -12,77 +18,119 @@
       role="dialog"
       aria-modal="true"
       :aria-labelledby="`${id}-dialog-label`"
+      :class="['vtd-date__wrapper', classes.wrapper]"
       @click="onClick"
       @keydown="onKeydown"
-      @keydown.tab="focusTrap"
+      @keydown.tab="onTab"
       @keydown.esc="show = false"
     >
-      <div class="header">
+      <div class="vts-date__navigation">
         <button
-          ref="prevYear"
-          class="prevYear"
+          :class="['vtd-date__prev-year', classes.prevYear]"
           aria-label="previous year"
+          type="button"
+          :disabled="disableNav.prevYear"
           @click="incrementYearBy(-1)"
         >
-          <slot name="prevYear">
+          <slot name="prevYearLabel">
             &#8606;
           </slot>
         </button>
         <button
-          class="prevMonth"
+          :class="['vtd-date__prev-month', classes.prevMonth]"
           aria-label="previous month"
+          type="button"
+          :disabled="disableNav.prevMonth"
           @click="incrementMonthBy(-1)"
         >
-          <slot name="prevMonth">
+          <slot name="prevMonthLabel">
             &#8592;
           </slot>
         </button>
-        <h4 :id="`${id}-dialog-label`" class="monthYear" aria-live="polite">
+        <h4
+          :id="`${id}-dialog-label`"
+          :class="['vtd-date__title', classes.title]"
+          aria-live="polite"
+        >
           {{ monthYear }}
         </h4>
-        <button class="" aria-label="next month" @click="incrementMonthBy(1)">
-          <slot name="nextMonth">
+        <button
+          :class="['vtd-date__next-month', classes.nextMonth]"
+          aria-label="next month"
+          type="button"
+          :disabled="disableNav.nextMonth"
+          @click="incrementMonthBy(1)"
+        >
+          <slot name="nextMonthLabel">
             &#8594;
           </slot>
         </button>
-        <button class="" aria-label="next year" @click="incrementYearBy(1)">
-          <slot name="nextYear">
+        <button
+          :class="['vtd-date__next-year', classes.nextYear]"
+          aria-label="next year"
+          type="button"
+          :disabled="disableNav.nextYear"
+          @click="incrementYearBy(1)"
+        >
+          <slot name="nextYearLabel">
             &#8608;
           </slot>
         </button>
       </div>
       <!-- eslint-disable-next-line vuejs-accessibility/no-redundant-roles -->
-      <table class="" role="grid" :aria-labelledby="`${id}-dialog-label`">
-        <thead>
-          <tr>
+      <table
+        :class="['vtd-date__calendar', classes.calendar]"
+        role="grid"
+        :aria-labelledby="`${id}-dialog-label`"
+      >
+        <thead :class="['vtd-date__thead', classes.thead]">
+          <tr :class="['vtd-date__week', classes.week]">
             <th
               v-for="(val, key) in daysOfWeek"
               :key="key"
               :abbr="val"
               scope="col"
+              :class="['vtd-date__th', classes.th]"
             >
               {{ key }}
             </th>
           </tr>
         </thead>
-        <tbody>
-          <tr v-for="week in 6" :key="week">
+        <tbody :class="['vtd-date__tbody', classes.tbody]">
+          <tr
+            v-for="week in 6"
+            :key="week"
+            :class="['vtd-date__tr', classes.tr]"
+          >
             <td
               v-for="day in daysByWeeks[week - 1]"
               :key="day.date.toString()"
               :class="[
-                'vts-date__cell',
-                { 'vts-date__cell--focused': day.isFocused },
+                'vts-date__td',
+                { 'vts-date__td--focused': day.isFocused },
+                { 'vts-date__td--selected': day.isSelected },
+                classes.td,
+                { [classes.tdFocused]: classes.tdFocused && day.isFocused },
+                { [classes.tdSelected]: classes.tdSelected && day.isSelected },
               ]"
             >
               <button
                 :class="[
                   'vts-date__day',
                   { 'vts-date__day--focused': day.isFocused },
+                  { 'vts-date__day--selected': day.isSelected },
+                  classes.day,
+                  { [classes.dayFocused]: classes.dayFocused && day.isFocused },
+                  {
+                    [classes.daySelected]:
+                      classes.daySelected && day.isSelected,
+                  },
                 ]"
                 :tabindex="day.isFocused ? '0' : '-1'"
                 :aria-selected="day.isFocused"
                 :value="day.date"
+                :disabled="day.disabled"
+                type="button"
               >
                 {{ day.date.getDate() }}
               </button>
@@ -91,23 +139,30 @@
         </tbody>
       </table>
 
-      <div class="">
-        <button class="" value="cancel" @click="show = false">
+      <!-- TODO: -->
+      <!-- <div class="">
+        <button class="" value="cancel" type="button" @click="show = false">
           Cancel
         </button>
-        <button class="" value="ok" @click="selectedDate = focusedDate">
+        <button
+          class=""
+          value="ok"
+          type="button"
+          @click="selectedDate = focusedDate"
+        >
           OK
         </button>
       </div>
       <div class="" aria-live="polite">
         Cursor keys can navigate dates
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
 
 <script>
 import KEYCODES from '../../data/keycodes.js';
+import { clickout } from '../../directives';
 import { randomString, applyFocusTrap } from '../../utils.js';
 
 /**
@@ -132,19 +187,35 @@ const keysUsed = [
   KEYCODES.PAGEDOWN,
   KEYCODES.HOME,
   KEYCODES.END,
+  KEYCODES.SHIFT,
 ];
 
+// Based on https://www.w3.org/TR/wai-aria-practices/examples/dialog-modal/datepicker-dialog.html
 export default {
-  // https://www.w3.org/TR/wai-aria-practices/examples/dialog-modal/datepicker-dialog.html
+  directives: {
+    clickout,
+  },
+
   model: {
     prop: 'date',
     event: 'update',
   },
 
   props: {
+    // TODO: include, exclude
     date: {
       type: [Date, String],
-      default: new Date(),
+      default: () => new Date(),
+    },
+
+    min: {
+      type: [Date, String],
+      default: '',
+    },
+
+    max: {
+      type: [Date, String],
+      default: '',
     },
 
     id: {
@@ -208,8 +279,25 @@ export default {
       } ${focusedDate.getFullYear()}`;
     },
 
+    disableNav() {
+      const { focusedDate, min, max } = this;
+      const disableNav = {};
+      const minDate = new Date(min);
+      const maxDate = new Date(max);
+
+      if (min) {
+        disableNav.prevYear = focusedDate.getYear() <= minDate.getYear();
+        disableNav.prevMonth = focusedDate.getMonth() <= minDate.getMonth();
+      }
+      if (max) {
+        disableNav.nextYear = focusedDate.getYear() >= maxDate.getYear();
+        disableNav.nextMonth = focusedDate.getMonth() >= maxDate.getMonth();
+      }
+      return disableNav;
+    },
+
     daysByWeeks() {
-      const { focusedDate, selectedDate } = this;
+      const { focusedDate, selectedDate, min, max } = this;
       const firstDayOfMonth = new Date(
         focusedDate.getFullYear(),
         focusedDate.getMonth(),
@@ -233,16 +321,40 @@ export default {
 
         for (let j = 0; j < 7; j++) {
           const date = new Date(d);
+          let disabled = false;
+          if (min) {
+            disabled = date < new Date(min);
+          }
+          if (max && !disabled) {
+            disabled = date > new Date(max);
+          }
+
           weeks[i].push({
             date,
             isFocused: sameDays(date, focusedDate),
             isSelected: sameDays(date, selectedDate),
+            disabled,
           });
           d.setDate(d.getDate() + 1);
         }
       }
 
       return weeks;
+    },
+
+    toggle() {
+      const { show } = this;
+      return {
+        bind: {
+          'aria-label': 'Select Date',
+          'aria-expanded': '' + show,
+        },
+        on: {
+          click: () => {
+            this.show = !show;
+          },
+        },
+      };
     },
   },
 
@@ -300,7 +412,7 @@ export default {
       event.stopPropagation();
       event.preventDefault();
 
-      const { focusedDate } = this;
+      const { focusedDate, min, max } = this;
       let d = new Date(focusedDate);
 
       switch (event.keyCode) {
@@ -349,32 +461,40 @@ export default {
           d.setDate(d.getDate() + (6 - d.getDay()));
           break;
       }
+
+      const minDate = min && new Date(min);
+      const maxDate = max && new Date(max);
+
+      if ((minDate && d < minDate) || (maxDate && d > maxDate)) return;
+
       this.focusedDate = d;
+
       this.$nextTick(() => {
         this.$el.querySelector('button[aria-selected="true"]').focus();
       });
     },
 
-    focusTrap(event) {
+    onTab(event) {
       applyFocusTrap(this.$refs.calendar, event);
+    },
+
+    onClickout(event) {
+      const { show } = this;
+      event.preventDefault();
+      if (show) {
+        this.show = false;
+      }
     },
   },
 };
 </script>
 
 <style>
-.datepicker {
+.vtd-date {
   position: relative;
 }
 
-.datepicker .datepickerDialog {
-  /* position: absolute;
-  width: 45%;
-  clear: both;
-  display: none; */
-}
-
-.datepicker .header {
+.vts-date__navigation {
   display: flex;
   justify-content: space-around;
 }
--- src/utils.js
+++ src/utils.js
@@ -1,45 +1,45 @@
-import FOCUSABLE from "./data/focusable.js"
+import FOCUSABLE from './data/focusable.js';
 
 export function randomString(
   length = 10,
-  allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  allowed = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 ) {
-  let result = ""
+  let result = '';
   for (let i = 0; i < length; i++) {
-    result += allowed.charAt(Math.floor(Math.random() * allowed.length))
+    result += allowed.charAt(Math.floor(Math.random() * allowed.length));
   }
-  return result
+  return result;
 }
 
 export function safeSlot(h, slot) {
-  return slot && slot.length > 1 ? h("div", slot) : slot
+  return slot && slot.length > 1 ? h('div', slot) : slot;
 }
 
 /**
  * @param {HTMLElement} el
  */
 export function applyFocusTrap(el, event) {
-  const focusable = Array.from(el.querySelectorAll(FOCUSABLE))
+  const focusable = Array.from(el.querySelectorAll(FOCUSABLE));
 
   if (!focusable.length) {
-    event.preventDefault()
-    return
+    event.preventDefault();
+    return;
   }
 
   if (!el.contains(document.activeElement)) {
-    event.preventDefault()
-    focusable[0].focus()
+    event.preventDefault();
+    focusable[0].focus();
   } else {
-    const focusedItemIndex = focusable.indexOf(document.activeElement)
+    const focusedItemIndex = focusable.indexOf(document.activeElement);
 
     if (event.shiftKey && focusedItemIndex === 0) {
-      focusable[focusable.length - 1].focus()
-      event.preventDefault()
+      focusable[focusable.length - 1].focus();
+      event.preventDefault();
     }
 
     if (!event.shiftKey && focusedItemIndex === focusable.length - 1) {
-      focusable[0].focus()
-      event.preventDefault()
+      focusable[0].focus();
+      event.preventDefault();
     }
   }
 }
