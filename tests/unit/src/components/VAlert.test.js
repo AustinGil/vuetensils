d9d7f6dc8f16240fc24951d49e91e743049c85bd
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2021-01-09 19:05:13 -0800

    adds tests

--- tests/unit/src/components/VAlert.test.js
+++ tests/unit/src/components/VAlert.test.js
@@ -1,13 +1,45 @@
-import { mount } from '@vue/test-utils';
+import { render } from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
 import VAlert from '@components/VAlert/VAlert.vue';
 
 describe('VAlert', () => {
-  describe('Created', () => {
+  describe('Default Functionality', () => {
 
-    it('renders correctly without props', () => {
-      const wrapper = mount(VAlert);
-      expect(wrapper).toMatchSnapshot();
+    it('render correctly without props', () => {
+      const { getByRole } = render(VAlert);
+      const alertBox = getByRole('alert');
+      expect(alertBox).toMatchSnapshot();
     });
 
+    describe('Dismiss & Timing visibility', () => {
+
+      it('render default dismiss button', () => {
+        const { getByLabelText } = render(VAlert, {
+          props: {
+            dismissible: true,
+          },
+        });
+  
+        const dismissButton = getByLabelText('Dismiss this alert');
+        expect(dismissButton).toBeVisible();
+      });
+
+      it('closes when dimiss button clicked', async () => {
+        const { findByRole, findByLabelText, debug } = render(VAlert, {
+          props: {
+            dismissible: true,
+          },
+        });
+        
+        let alertBox = await findByRole('alert');
+        let dismissButton = await findByLabelText('Dismiss this alert');
+        debug(alertBox);
+        expect(alertBox).toBeVisible();
+        userEvent.click(dismissButton);
+        debug();
+        expect(alertBox).not.toBeVisible();
+      });
+
+    });
   });
 });
