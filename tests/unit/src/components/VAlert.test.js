eefa115d828d3172f8d08d49fc32e9212b56513d
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2021-01-10 13:02:15 -0800

    adds a failing test

--- tests/unit/src/components/VAlert.test.js
+++ tests/unit/src/components/VAlert.test.js
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/vue';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import VAlert from '@components/VAlert/VAlert.vue';
 
 describe('VAlert', () => {
@@ -24,6 +24,23 @@ describe('VAlert', () => {
         expect(dismissButton).toBeVisible();
       });
 
+      it('dismisses @default slot content', () => {
+        const { getByText, getByLabelText } = render(VAlert, {
+          props: {
+            dismissible: true,
+          },
+          slots: {
+            default: `<span>Testing Content in @default slot</span>`
+          }
+        });
+  
+        const alertBox = getByText('Testing Content in @default slot');
+        const dismissButton = getByLabelText('Dismiss this alert');
+        
+        expect(alertBox).toBeVisible();
+        userEvent.click(dismissButton);        
+        expect(alertBox).not.toBeVisible();
+      });
     });
   });
 });
