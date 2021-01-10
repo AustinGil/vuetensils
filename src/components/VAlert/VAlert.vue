28167a64b55d6b63c902ae4b1f21b3a8dd182a29
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2021-01-09 18:50:02 -0800

    add notes

--- src/components/VAlert/VAlert.vue
+++ src/components/VAlert/VAlert.vue
@@ -26,6 +26,18 @@
 
 <script>
 /**
+ * NOTES Concerning :visible prop. 
+ * Per W3C specifications:
+ * https://www.w3.org/TR/wai-aria-practices/#alert
+ * --
+ * "It is also important to avoid designing alerts that disappear automatically. 
+ * An alert that disappears too quickly can lead to failure to meet WCAG 2.0 success criterion 2.2.3. 
+ * Another critical design consideration is the frequency of interruption caused by alerts. 
+ * Frequent interruptions inhibit usability for people with visual and cognitive disabilities, 
+ * which makes meeting the requirements of WCAG 2.0 success criterion 2.2.4 more difficult."
+ */
+
+/**
  * A simple component for notifiying users of specific information. Good for informative snippets, error messages, and more. It can be shown or hidden dynamically, and even supports auto-hiding after a given time.
  */
 export default {
