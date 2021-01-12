edd021528cbef465b6dcd2a35c59f9703904f2a7
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2021-01-11 17:26:12 -0800

    refactor: adds 5 passing tests

--- tests/unit/src/components/VAction.test.js
+++ tests/unit/src/components/VAction.test.js
@@ -1,54 +1,44 @@
 import { render } from '@testing-library/vue';
-import { createLocalVue } from '@vue/test-utils';
-import VueRouter from 'vue-router';
-
-// import userEvent from '@testing-library/user-event';
+import { RouterLinkStub } from '@vue/test-utils';
 import VActionStub from '../stubs/VActionStub.vue';
 
 describe('VAction Unit Test', () => {
   describe('Default Functionality', () => {
     it('Returns a <RouterLink/> tag', () => {
-    // Create localInstance of VueRouter to get access to <router-link /> component
-      const localVue = createLocalVue();
-      localVue.use(VueRouter);
-      const { getByTestId, debug } = render(VActionStub, {
-        localVue,
-        // props: {
-        //   to: '/internal-link',
-        // },
-        // data: function() {
-        //   return {
-        //     attrs: {
-        //       'data-testid': 'internal',
-        //     },
-        //   };
-        // },
-        // slots: {
-        //   default: 'Internal Link',
-        // },
-        stubs: ['router-link'],
+      const { getByTestId } = render(VActionStub, {
+        stubs: {
+          RouterLink: RouterLinkStub
+        },
       });
 
       const testLink = getByTestId('internal');
-      debug(testLink);
       expect(testLink).toHaveAttribute(
         'to',
         expect.stringContaining('/internal-link')
       );
     });
-    it('Returns an anchor tag', () => {
-      const { getByTestId, debug } = render(VActionStub);
-      const testLink = getByTestId('external');
-      debug(testLink);
+    it('Returns an anchor tag with :href', () => {
+      const { getByTestId } = render(VActionStub);
+      const testLink = getByTestId('external-href');
       expect(testLink).toHaveAttribute(
         'href',
         expect.stringContaining('http://example.com/')
       );
     });
+    it('Fails to return an anchor tag with :attrs', () => {
+      const { getByTestId } = render(VActionStub);
+      const testLink = getByTestId('external-attrs');
+      expect(testLink).not.toHaveAttribute('href');
+    });
+    it('Fails to return an anchor tag with :data', () => {
+      const { getByTestId } = render(VActionStub);
+      const testLink = getByTestId('external-data');
+      expect(testLink).not.toHaveAttribute('href');
+    });
     it('Returns a <button />', () => {
       const { getByTestId } = render(VActionStub);
       const testLink = getByTestId('button');
-      expect(testLink).toContainHTML('<button class="vts-action" />');
+      expect(testLink).toHaveTextContent('Button');
     });
   });
 });
