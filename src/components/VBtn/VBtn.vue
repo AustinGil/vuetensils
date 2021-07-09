2f4c8f77279e262a67f3a27ce42013dd98c409aa
Austin Gil <austin@stegosource.com>
2021-07-09 16:15:41 -0700

    Refactoring for v3 support

--- src/components/VBtn/VBtn.vue
+++ src/components/VBtn/VBtn.vue
@@ -1,45 +1,36 @@
-<script>
-/**
- * Detects if a VDOM element is a <RouterLink>, <a>, or <button>
- *
- * @param {import('vue').PropOptions & { to?:string }} props props container
- * @param {import('vue').VNodeData} data data container
- * @return {'RouterLink' | 'a' | 'button'} 'RouterLink', 'a', or 'button'
- */
-export function getTag(props, data) {
-  if (props && props.to) {
-    return 'RouterLink';
-  }
-  if (data && data.attrs && data.attrs.href) {
-    return 'a';
-  }
-  return 'button';
-}
+<template>
+  <component :is="tag" v-bind="attrs" class="vts-action vts-btn'">
+    <slot />
+  </component>
+</template>
 
+<script>
 export default {
   name: 'VBtn',
-  functional: true,
-  render(h, { data, listeners, props, children }) {
-    data.attrs = data.attrs || {};
-    const tag = getTag(props, data);
-    const options = {
-      ...data,
-      props,
-      class: ['vts-action vts-btn', data.class],
-      on: listeners,
-    };
-
-    if (tag === 'RouterLink') {
-      options.nativeOn = listeners;
-    }
-    if (data.attrs.target === '_blank') {
-      options.attrs.rel = 'noopener';
-    }
-    if (tag === 'button') {
-      options.attrs.type = options.attrs.type || 'button';
-    }
-
-    return h(tag, options, children);
+  props: {
+    to: {
+      type: [String, Object],
+      default: '',
+    },
+  },
+  computed: {
+    tag() {
+      const { to, $attrs } = this;
+      if (to) {
+        return 'RouterLink';
+      }
+      if ($attrs && $attrs.href) {
+        return 'a';
+      }
+      return 'button';
+    },
+    attrs() {
+      const attrs = { ...this.$attrs };
+      if (this.tag === 'button') {
+        attrs.type = attrs.type || 'button';
+      }
+      return attrs;
+    },
   },
 };
 </script>
